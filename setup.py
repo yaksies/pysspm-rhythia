c01c2f4ef7fe8419ab4ee55ad563967da666387c
David Jed <Dev.DavidJed@gmail.com>
2024-08-17 03:24:47 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pysspm",  # Replace with your package name
+    name="pysspm",
     version="0.1.0",  # Initial release version
     author="David Jedlovsky",
     author_email="Dev.DavidJed@gmail.com",
-    description="A Python library dedicated to reading, writing, and modifying the SSPM file format",
+    description="A Python library dedicated to reading, writing, and modifying the Rhythia SSPM file format",
     long_description=open("README.md", encoding="UTF-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/David-Jed/pysspm",  # Replace with your project'As URL
-    packages=find_packages(),  # Automatically finds your package
-    classifiers=[
+    url="https://github.com/David-Jed/pysspm",
+    packages=find_packages(),
+    classifiers = [
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",  # Adjust based on your needs
+    python_requires=">=3.7",
     install_requires=[
         "numpy",
     ],
     extras_require={
         "dev": [
-            "pytest",  # Add dev dependencies here
+            "pytest",
         ],
     },
+    keywords=["Rhythia", "Sound space", "SSPM", "Rhythm game"]
     include_package_data=True,
 )
