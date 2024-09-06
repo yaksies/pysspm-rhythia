2cf0c986df8f013da2c0e39dc3ece19d934e34b3
David Jed <Dev.DavidJed@gmail.com>
2024-09-06 19:35:58 -0400

    saving basic implementation for testing library

--- tests/test_sspmLib.py
+++ tests/test_sspmLib.py
@@ -7,6 +7,33 @@ import glob
 
 parser = SSPMParser()
 
+# function for using pytest
+def test_read_sspm(): # Basic reading of sspm file format
+    parser = SSPMParser()
+    parser.ReadSSPM("./tests/Test.sspm")
+    assert len(parser.Notes) > 0
+
+# now we read and write the notes to a new sspm file.
+def test_write_sspm(): # Writing a new SSPM file from parsed notes
+    parser = SSPMParser()
+    first = parser.ReadSSPM("./tests/Test.sspm")
+    parser.WriteSSPM("./tests/Test2.sspm", mapName="KOCMOC_MAP_TESTING_SSPMLIB")
+    second = parser.ReadSSPM("./tests/Test.sspm")
+
+    # comparing values between first and second. should all be the same
+    assert first.audioBytes == second.audioBytes
+    print("Audio checked")
+    assert first.Notes == second.Notes
+    print("Notes checked")
+    assert first.lastMs == second.lastMs
+    assert first.coverBytes == second.coverBytes
+    
+# This last test starts from scratch, and applies all the values into the write function.
+def test_write_sspm_from_scratch(): # Writing a new SSPM file from scratch
+    parser = SSPMParser()
+    parser.ReadSSPM("./tests/Test.sspm")
+
+# Read the SSPM file and parse it into a list of notes.
 parser.ReadSSPM("./tests/Test.sspm")
 print(len(parser.Notes))
 print(parser.Notes[1000:1020])
